0471c04040419728eebaa0e65ce7743c23e2bff2
s-nguyen <nereuxie@gmail.com>
2015-11-12 14:26:37 -0700

    idk im confuusd and mass comiting

--- hlsyn/hlsyn/FileIO.h
+++ hlsyn/hlsyn/FileIO.h
@@ -5,17 +5,13 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include "Module.h"
-#include "Bits.h"
-#include "Variable.h"
+
 
 using namespace std;
 
-int writefile(char* file, vector<Variable> &v, vector<Module> &top);
-Bits parseInt(string s);
-int readfile(char* file, vector<Variable> &v, vector<Module> &top);
-bool areSize(int s, vector<Variable> &v, string t);
-int checkVar(vector<Variable> &v, vector<Module> &top);
+
+int readfile(char* file);
+
 
 
 
--- hlsyn/hlsyn/hlsyn.cpp
+++ hlsyn/hlsyn/hlsyn.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include "Bits.h"
 #include "FileIO.h"
-#include "Variable.h"
+
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	vector<Variable> var;
-	readfile(argv[1], var);
+
 	return 0;
 }
