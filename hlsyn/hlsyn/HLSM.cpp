116462c33223d1dfd19d8e1cdc69a99d36daa908
WellMet <sruiz4690@gmail.com>
2015-11-16 20:07:44 -0700

    finished hlsm need testing

--- hlsyn/hlsyn/HLSM.cpp
+++ hlsyn/hlsyn/HLSM.cpp
@@ -1,6 +1,6 @@
 #include "HLSM.h"
 #include <math.h>
-
+#include <sstream>
 
 
 HLSM::HLSM()
@@ -274,20 +274,20 @@ void HLSM::createHeader(ofstream &w_file) {
 
 int HLSM::createStateReg(ofstream &w_file) {
 	//calculate number of states (states = maxmim time for each schedle + wait + done)
-	int numStates = 2;
+	int numStates = 0;
 	for (int i = 0; i < schedules.size(); i++) {
-		numStates = numStates + schedules[i]->getVertices().back()->getEndTime();
+		numStates = numStates + schedules[i]->getVertices().back()->getTime() + schedules[i]->getVertices().back()->getDelay() - 1;
 	}
 
 	//take log base 2 of number of states, then call ceil() to round up to next int
-	int bitWidth = ceil(log2(numStates)) - 1;
+	int bitWidth = ceil(log2(numStates+2)) - 1;
 
 	//write state names
 	int nameCount = 0;
 	vector<string> names;
 	names.push_back("STATE_WAIT");
 
-	for (int i = 1; i < numStates - 1; i++) {
+	for (int i = 1; i < numStates + 2 - 1; i++) {
 		names.push_back("STATE_" + to_string(i));
 	}
 
@@ -308,61 +308,104 @@ int HLSM::createHSM(char* file) {
 	ofstream w_file(file);
 
 	if (w_file) {
+		int vertex = 0;
+		int schedule = 0;
+		int cycle = 1;
+		bool opFound = false;
+		stringstream opLine;
+		int latestVertex = 0;
+		int latestSchedule = 0;
+		
+		//write module
 		createHeader(w_file);
-		int numStates = createStateReg(w_file);
+		//write state header
+		int states = createStateReg(w_file);
+		stringstream stateLogic;
 
-		//create state logic
-		w_file << endl << "\talways@ (posedge Clk) begin" << endl;
-		w_file << "\t\tif (Rst == 1'b1) begin" << endl << "\t\t\tState <= STATE_WAIT;" << endl << "\t\t\tDone <= 0;" << endl;
+		//create always @
+		stateLogic << endl << "\talways@ (posedge Clk) begin" << endl;
+		stateLogic << "\t\tif (Rst == 1'b1) begin" << endl << "\t\t\tState <= STATE_WAIT;" << endl << "\t\t\tDone <= 0;" << endl;
 		//assign all outputs zero
 		for (int i = 0; i < allVars.size(); i++) {
 			if (allVars[i]->getType().compare("output") == 0) {
-				w_file << "\t\t\t" << allVars[i]->getVar() << " <= 0;" << endl;
+				stateLogic << "\t\t\t" << allVars[i]->getVar() << " <= 0;" << endl;
 			}
 		}
-		w_file << "\t\tend" << endl << "\t\telse begin" << endl;
-		w_file << "\t\tcase (State)" << endl;
+		stateLogic << "\t\tend" << endl << "\t\telse begin" << endl;
+		stateLogic << "\t\tcase (State)" << endl;
 		
 		//write wait state
-		w_file << "\t\t\tSTATE_WAIT: begin" << endl << "\t\t\t\tif (Start = 1'b1) begin" << endl << "\t\t\t\t\tState = STATE_1;" << endl << "\t\t\t\tend" << endl << "\t\t\t\telse begin" << endl <<
+		stateLogic << "\t\t\tSTATE_WAIT: begin" << endl << "\t\t\t\tDone <= 0;" << endl << "\t\t\t\tif (Start = 1'b1) begin" << endl << "\t\t\t\t\tState = STATE_1;" << endl << "\t\t\t\tend" << endl << "\t\t\t\telse begin" << endl <<
 			"\t\t\t\t\tState = STATE_WAIT;" << endl << "\t\t\t\tend" << endl << "\t\t\tend" << endl;
 
 		//write operations states
-		int vertex = 0;
-		int schedule = 0;
-		int latestVertex;
-		int latestSchedule;
-		int actualState;
-		for (int i = 1; i < numStates - 1; i++) {
-			w_file << "\t\t\tSTATE_" << i << ": begin" << endl;
-			latestVertex = -1;
-			latestSchedule = -1;
-			for (schedule = 0; schedule < schedules.size(); schedule++) { //iterate through each schedule 
-				for (vertex = 0; vertex < schedules[schedule]->getVertices().size(); vertex++) { //iterate through each vertex in the schedule to search for a vertex that starts at [i]
-					//if the vertex start time (normalized with the number of states) is the same as the state counter, print the operations into that state
-					actualState = getPrevCycles(schedule);
-					if (schedules[schedule]->getVertices()[vertex]->getBeginTime() + actualState == i && schedules[schedule]->getVertices()[vertex]->getType().compare("?") == 0) {//mux
-						w_file << "\t\t\t\t" << schedules[schedule]->getVertices()[vertex]->getOutput().getVar() << " <= " << "sel " << schedules[schedule]->getVertices()[vertex]->getType() <<
+		for (int i = 1; i <= states; i++) { //for each state check to see if a vertex starts or ends in it
+			stateLogic << "\t\t\tSTATE_" << i << ": begin" << endl;
+			for (schedule = 0; schedule < schedules.size(); schedule++) {
+				cycle = i - getPrevCycles(schedule);
+				for (vertex = 0; vertex < schedules[schedule]->getVertices().size(); vertex++) {
+					//find all vertexes with start time = current state and add to operation stringbuffer
+					if (schedules[schedule]->getVertices()[vertex]->getTime() == cycle && schedules[schedule]->getVertices()[vertex]->getType().compare("?") == 0) {//if vertex start time = state and vertex is mux
+						opLine << "\t\t\t\t" << schedules[schedule]->getVertices()[vertex]->getOutput().getVar() << " <= " << "sel " << schedules[schedule]->getVertices()[vertex]->getType() <<
 							" " + schedules[schedule]->getVertices()[vertex]->getInput1().getVar() << " : " + schedules[schedule]->getVertices()[vertex]->getInput2().getVar() + ";" << endl;
-						latestVertex = vertex;
-						latestSchedule = schedule;
 					}
-					else if (schedules[schedule]->getVertices()[vertex]->getBeginTime() + actualState == i && schedules[schedule]->getVertices()[vertex]->getType().compare("?") != 0) {//any other op
-						w_file << "\t\t\t\t" << schedules[schedule]->getVertices()[vertex]->getOutput().getVar() << " <= " << schedules[schedule]->getVertices()[vertex]->getInput1().getVar() <<
+					else if (schedules[schedule]->getVertices()[vertex]->getTime() == cycle && schedules[schedule]->getVertices()[vertex]->getType().compare("?") != 0) { //any other op
+						opLine << "\t\t\t\t" << schedules[schedule]->getVertices()[vertex]->getOutput().getVar() << " <= " << schedules[schedule]->getVertices()[vertex]->getInput1().getVar() <<
 							" " + schedules[schedule]->getVertices()[vertex]->getType() << " " + schedules[schedule]->getVertices()[vertex]->getInput2().getVar() + ";" << endl;
+					}
+
+					//check if vertex ends this state
+					int endsTime = schedules[schedule]->getVertices()[vertex]->getTime() + schedules[schedule]->getVertices()[vertex]->getDelay() -1 + getPrevCycles(schedule);
+					if (endsTime == i) {
 						latestVertex = vertex;
 						latestSchedule = schedule;
+						opFound = true;
 					}
 				}
 			}
+			//write state ops
+			stateLogic << opLine.str();
+			opLine.str("");
+
 			//determine next state
+			//if this is the last vertex in the schedule and this isnt the last schedule
+			if ((latestVertex == schedules[latestSchedule]->getVertices().size()-1) && latestSchedule < schedules.size() - 1 && opFound) {
+				if (schedules[latestSchedule + 1]->getVertices()[0]->getConditionIF().compare("no condition") != 0) {//check to see if entering if
+																											   //code has reached an if statement, now check how many cycles the if lasts for
+					int ifcycles = schedules[latestSchedule + 1]->getVertices().back()->getTime() + schedules[latestSchedule + 1]->getVertices().back()->getDelay() - 1;
+					stateLogic << endl << "\t\t\t\tif (" << schedules[latestSchedule + 1]->getVertices()[0]->getConditionIF() << ") begin" << endl << "\t\t\t\t\tState = STATE_" <<
+						i + 1 << ";" << endl << "\t\t\t\tend" << endl << "\t\t\t\telse begin" << endl <<
+						"\t\t\t\t\tState = STATE_" << i + 1 + ifcycles << ";" << endl << "\t\t\t\tend" << endl << "\t\t\tend" << endl;
+					opFound = false;
+				}
+				else if (schedules[latestSchedule + 1]->getVertices()[0]->getConditionIF().compare("no condition") == 0) { //leaving a branch, just go to next vertex
+					stateLogic << endl << "\t\t\t\tState <= STATE_" << i + 1 << ";" << endl << "\t\t\tend" << endl;
+					opFound = false;
+				}
+			}
+			else if ((latestVertex < schedules[latestSchedule]->getVertices().size()-1) && opFound) {//else if this is a vertex in the middle of a schedule go to next state
+				stateLogic << endl << "\t\t\t\tState <= STATE_" << i + 1 << ";" << endl << "\t\t\tend" << endl;
+				opFound = false;
+			}
+			else if ((latestVertex == schedules[latestSchedule]->getVertices().size()-1) && (latestSchedule == schedules.size()-1) && opFound){ //else this is the last vertex and schedule, go to done state
+				stateLogic << endl << "\t\t\t\tState <= STATE_FINAL;" << endl << "\t\t\tend" << endl;
+				opFound = false;
+			}
+			else if (!opFound) { //else if no operation finishes this cycle just go to next state
+				stateLogic << endl << "\t\t\t\tState <= STATE_" << i + 1 << ";" << endl << "\t\t\tend" << endl;
+			}
 		}
 
 		//write final state
-		w_file << "\t\t\tSTATE_FINAL: begin" << endl << "\t\t\t\tDone <= 1'b1;" << endl << "\t\t\t\tState <= STATE_WAIT;" << endl << "\t\t\tend" << endl;
+		stateLogic << "\t\t\tSTATE_FINAL: begin" << endl << "\t\t\t\tDone <= 1'b1;" << endl << "\t\t\t\tState <= STATE_WAIT;" << endl << "\t\t\tend" << endl;
 
 		//write default
-		w_file << "\t\t\tdefault: State <= STATE_WAIT;" << endl;
+		stateLogic << "\t\t\tdefault: State <= STATE_WAIT;" << endl;
+
+
+		
+		//write state logic
+		w_file << stateLogic.str();
 
 		//finish off module
 		w_file << "\t\tendcase" << endl; //endcase
@@ -395,7 +438,7 @@ int HLSM::getPrevCycles(int schedule) {
 	}
 	else{
 		for (int k = 0; k < schedule; k++) {
-			cycles = cycles + schedules[k]->getVertices().back()->getEndTime() - 1;
+			cycles = cycles + schedules[k]->getVertices().back()->getTime() + schedules[k]->getVertices().back()->getDelay() - 1;
 		}
 		return cycles;
 	}
